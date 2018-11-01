e42c25a3d2b3fa6024ee106f8e3f1df9d5d22af9
github username <github email address>
2018-11-01 00:13:21 -0400

    Done.

---
+++ lib/artist.rb
@@ -0,0 +1,15 @@
+# class Song
+#   attr_accessor :title
+#
+#   def initialize(title)
+#     @title = title
+#   end
+# end
+
+class Artist
+  attr_accessor :name
+
+  def name
+    @name
+  end
+end
